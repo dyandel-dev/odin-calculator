f9030e55d61e6e18ada313a1adf13d7303b69b69
JD Alegre Casao <117078422+dyandel-dev@users.noreply.github.com>
2025-10-27 20:15:17 +0800

    Implement initial basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function addNumber(first_num, second_num) {
+    return first_num + second_num;
+}
+
+function subtractNumber(first_num, second_num) {
+    return first_num - second_num;
+}
+
+function multiplyNumber(first_num, second_num) {
+    return first_num * second_num;
+}
+
+function divideNumber(first_num, second_num) {
+    return first_num / second_num;
+}
