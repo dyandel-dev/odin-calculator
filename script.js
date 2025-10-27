28fee22d3e2b0b8a60c64c56f583c4a977dfad76
JD Alegre Casao <117078422+dyandel-dev@users.noreply.github.com>
2025-10-27 20:36:03 +0800

    Create function to handle math operations

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function multiplyNumber(first_num, second_num) {
 function divideNumber(first_num, second_num) {
     return first_num / second_num;
 }
+
+let first_num;
+let second_num;
+let math_operation;
+
+function operate(first_num, math_operation, second_num) {
+    
+    switch(math_operation) {
+        case '+':
+            return addNumber(first_num, second_num);
+            break;
+        case '-':
+            return subtractNumber(first_num, second_num);
+            break;
+        case '*':
+            return multiplyNumber(first_num, second_num);
+            break;
+        case '/':
+            return divideNumber(first_num, second_num);
+            break;
+        default:
+            alert("Error!")
+    }
+}
